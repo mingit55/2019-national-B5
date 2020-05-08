70be36166967603b27114912875aba6dbdea3cb3
mingit55 <mingit55@gmail.com>
2020-05-08 17:16:19 +0900

    add rect clip

---
+++ js/participate/Rect.js
@@ -0,0 +1,50 @@
+class Rect extends Clip {
+    constructor(){
+        super(...arguments);
+
+        this.processing = true;
+        this.temp = [0, 0];
+        this.data = { x: 0, y: 0, w: 0, h: 0 };
+    }
+
+    mouseDown(e){
+        let [X, Y] = this.getXY(e);
+        this.data.x = X;
+        this.data.y = Y;
+        this.temp = [X, Y];
+    }
+
+    mouseMove(e){
+        let [fx, fy] = this.temp;
+        let [X, Y] = this.getXY(e);
+        let {x, y} = this.data;
+
+        let w = X - fx;
+        let h = Y - fy;
+
+        if(w < 0){
+            x = X;
+            w = fx - X;
+        }
+
+        if(h < 0){
+            y = Y;
+            h = fy - Y;
+        }
+
+        this.data = {x, y, w, h};
+    }
+
+    mouseUp(){
+        this.processing = false;
+        this.viewer.clear();
+    }
+
+    redraw(){
+        let {x, y, w, h} = this.data;
+        this.ctx.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
+
+        if(this.processing) this.ctx.strokeRect(x, y, w, h);
+        else this.ctx.fillRect(x, y, w, h);
+    }
+}
